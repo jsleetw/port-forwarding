fb0d6e61c1730970b84b92c6ef86c4b7ee8c5cff
jsleetw <ail529@gmail.com>
2012-01-31 11:41:36 +0800

    add a sqlite3 db class

--- db.py
+++ db.py
@@ -1,6 +1,7 @@
 import sqlite3
 import sys
 
+
 class db():
     """db"""
     def __init__(self, argv):
@@ -9,8 +10,25 @@ class db():
             sys.exit()
 
         print argv[1]
+        action = argv[1]
         con = sqlite3.connect("./test.db")
+        con.isolation_level = None  # for autocommit mode
         self.cur = con.cursor()
+        getattr(self, action)()
+
+    def insert(self):
+        self.cur.execute("insert into test(i) values (1)")
+        self.cur.execute("select * from test")
+        print self.cur.fetchall()
+
+    def select(self):
+        try:
+            self.cur.execute("select * from test")
+            print self.cur.fetchall()
+        except sqlite3.OperationalError, e:
+            print "E:" + str(e)
+            self.create()
+            self.select()
 
     def create(self):
         self.cur.execute("create table test(i)")
